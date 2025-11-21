e5538936e294971b5465da8f6398d5324f3ff591
jacksonbai <jacksonbai2004@outlook.com>
2025-11-20 18:54:02 -0600

    Update my code.

--- SafePathRN/config/firebaseConfig.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyCoGLyuKPVUKJP2dxkbrgPz7gHZZdRR2S8",
-  authDomain: "safepath-73a05.firebaseapp.com",
-  projectId: "safepath-73a05",
-  storageBucket: "safepath-73a05.firebasestorage.app",
-  messagingSenderId: "386641254598",
-  appId: "1:386641254598:web:0496b3f3bb99c070ccdfc7",
-} as const;
